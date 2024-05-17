c19f9557f33daa240b37c087c9841c7966145bc0
kereszthury <kereszthurypeter@gmail.com>
2024-05-17 10:36:13 +0200

    First mock implementation

--- src/test/java/hu/bme/mit/spaceship/GT4500Test.java
+++ src/test/java/hu/bme/mit/spaceship/GT4500Test.java
@@ -10,31 +10,46 @@ public class GT4500Test {
 
   private GT4500 ship;
 
+private TorpedoStore primary, secondary;
+
   @BeforeEach
   public void init(){
-    this.ship = new GT4500();
+    primary = mock(TorpedoStore.class);
+    secondary =  mock(TorpedoStore.class);
+
+    this.ship = new GT4500(primary, secondary);
   }
 
   @Test
   public void fireTorpedo_Single_Success(){
     // Arrange
+    when(primary.fire(1)).thenReturn(true);
+    when(secondary.fire(1)).thenReturn(false);
 
     // Act
     boolean result = ship.fireTorpedo(FiringMode.SINGLE);
 
     // Assert
     assertEquals(true, result);
+
+    verify(primary, times(1)).fire(1);
+    verify(primary, times(0)).fire(1);
   }
 
   @Test
   public void fireTorpedo_All_Success(){
     // Arrange
+    when(primary.fire(1)).thenReturn(true);
+    when(secondary.fire(1)).thenReturn(false);
 
     // Act
     boolean result = ship.fireTorpedo(FiringMode.ALL);
 
     // Assert
     assertEquals(true, result);
+
+    verify(primary, times(1)).fire(1);
+    verify(primary, times(1)).fire(1);
   }
 
 }
